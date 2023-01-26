e578d75102ffb7977b94a74bddd478c27a1da018
Nikolay Dementev <dementevni@mail.ru>
2023-01-26 11:57:22 +0500

    BonusServiceTest changes

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -49,4 +49,20 @@ public class BonusServiceTest {
         // производим проверку (сравниваем ожидаемый и фактический):
         assertEquals(expected, actual);
     }
+
+    @org.junit.jupiter.api.Test
+    void shouldCalculateForUnregisteredAndOverLimit() {
+        BonusService service = new BonusService();
+
+        // подготавливаем данные:
+        long amount = 1_000_000;
+        boolean unregistered = false;
+        long expected = 500;
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, unregistered);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        assertEquals(expected, actual);
+    }
 }
